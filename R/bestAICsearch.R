34174930bba0bc98783e86a6189a4159e92662a4
PhysFritz <fritzbayer@ymail.com>
2022-03-21 16:33:26 +0100

    added AIC without plots

--- R/bestAICsearch.R
+++ R/bestAICsearch.R
@@ -1,7 +1,23 @@
-#' @import ggplot2
+#' @title netClusterParallel
+#'
+#' @description Network-based clustering of multiple seeds using parallel computing
+#'
+#' @param binaryMatrix Data to be clustered
+#' @param minK Min number of clusters
+#' @param maxK Max number of clusters
+#' @param chiVec Vector of chi values
+#' @param startseed Seed
+#' @param nIterations Number of iterations
+#' @param AICrange AIC range
+#'
+#' @return list of AIC scrores
+#' @export
+#'
+# #' @import ggplot2
 #' @importFrom reshape2 melt
 #' @importFrom grDevices rgb
-bestAICsearch <- function(binaryMatrix, minK = 2, maxK, chiVec, startseed = 100, nIterations = 50,AICrange=100) {
+#'
+bestAICsearch <- function(binaryMatrix, minK = 2, maxK=5, chiVec=c(0,0.5,1,2,3), startseed = 100, nIterations = 50,AICrange=100) {
 #
 #     require('ggplot2')
 #     require('reshape2')
@@ -66,36 +82,36 @@ bestAICsearch <- function(binaryMatrix, minK = 2, maxK, chiVec, startseed = 100,
 
     #middycol<-c(0.8,0.2,0)
 
-    # ggheatmap<-ggplot(data = meltdivergy, aes(Var1, Var2, fill = value))+
-    ggheatmap<-ggplot(data = meltdivergy)+
-        geom_tile() +
-        xlab(expression(chi)) +
-        ylab("k") +
-        scale_fill_gradient2(high =rgb(0.98,0.98,1), low = rgb(0,0.35,0.8),
-                             mid=rgb(0.49,0.665,0.9),space="Lab",na.value="grey75",
-                             midpoint=topaics/2,limit = c(0,topaics), name="AIC\nchange\n") +
-        scale_y_continuous(breaks=c(minK:maxK)) +
-        theme_minimal() +
-        theme(axis.title.x = element_text(vjust=-1),axis.title.y = element_text(angle=0,hjust=-0.5,vjust=0.505)) +
-        theme(axis.text.x = element_text(angle = 0, vjust = 0.5,size = 20, hjust = 0.6),
-              axis.text.y = element_text(angle = 0, vjust = 0.5,size = 20, hjust = 1),
-              legend.text=element_text(size=20),
-              axis.title=element_text(size=30),
-              legend.title=element_text(size=24))+theme(legend.key.size = unit(2,"line")) +
-        theme(plot.margin=unit(c(-0.3,-0.3,0.4,0.4),"cm"))
-
-    print(ggheatmap)
-
-    #pdf(paste("heatmapaic.pdf",sep=""), width=7.5, height=6, onefile=F, pointsize=10,  paper="special")
-
-    ggheatmap +
-        theme(
-            #axis.title.x = element_text("prune and reattach probability"),
-            #axis.title.y = element_text("swap two nodes probability"),
-            panel.grid.major = element_blank(),
-            panel.border = element_blank(),
-            panel.background = element_blank(),
-            axis.ticks = element_blank())
+    # ggheatmap<-ggplot(data = meltdivergy, aes(meltdivergy$Var1, meltdivergy$Var2, fill = meltdivergy$value))+
+    # # ggheatmap<-ggplot(data = meltdivergy)+
+    #     geom_tile() +
+    #     xlab(expression(chi)) +
+    #     ylab("k") +
+    #     scale_fill_gradient2(high =rgb(0.98,0.98,1), low = rgb(0,0.35,0.8),
+    #                          mid=rgb(0.49,0.665,0.9),space="Lab",na.value="grey75",
+    #                          midpoint=topaics/2,limit = c(0,topaics), name="AIC\nchange\n") +
+    #     scale_y_continuous(breaks=c(minK:maxK)) +
+    #     theme_minimal() +
+    #     theme(axis.title.x = element_text(vjust=-1),axis.title.y = element_text(angle=0,hjust=-0.5,vjust=0.505)) +
+    #     theme(axis.text.x = element_text(angle = 0, vjust = 0.5,size = 20, hjust = 0.6),
+    #           axis.text.y = element_text(angle = 0, vjust = 0.5,size = 20, hjust = 1),
+    #           legend.text=element_text(size=20),
+    #           axis.title=element_text(size=30),
+    #           legend.title=element_text(size=24))+theme(legend.key.size = unit(2,"line")) +
+    #     theme(plot.margin=unit(c(-0.3,-0.3,0.4,0.4),"cm"))
+    #
+    # print(ggheatmap)
+    #
+    # #pdf(paste("heatmapaic.pdf",sep=""), width=7.5, height=6, onefile=F, pointsize=10,  paper="special")
+    #
+    # ggheatmap +
+    #     theme(
+    #         #axis.title.x = element_text("prune and reattach probability"),
+    #         #axis.title.y = element_text("swap two nodes probability"),
+    #         panel.grid.major = element_blank(),
+    #         panel.border = element_blank(),
+    #         panel.background = element_blank(),
+    #         axis.ticks = element_blank())
 
     #dev.off()
     return(output)
