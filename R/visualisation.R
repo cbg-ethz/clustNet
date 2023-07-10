c9ed86a3eddf5bf22ffab127c5af5c6295633cb4
PhysFritz <fritzbayer@ymail.com>
2023-07-10 10:56:51 +0200

    added frequency plot

--- R/visualisation.R
+++ R/visualisation.R
@@ -62,6 +62,16 @@ nice_DAG_plot <- function(my_DAG, print_direct=TRUE, node_size=NULL, CPDAG=TRUE,
 }
 
 
+# Compute by entropy
+entropy <- function(cat.vect){
+  # from https://stats.stackexchange.com/questions/221332/variance-of-a-distribution-of-multi-level-categorical-data
+  px  = table(cat.vect)/length(cat.vect)
+  lpx = log(px, base=2)
+  ent = -sum(px*lpx)
+  return(ent)
+}
+
+
 #' @title plot_clusters
 #'
 #' @description Plot clusters
@@ -69,19 +79,18 @@ nice_DAG_plot <- function(my_DAG, print_direct=TRUE, node_size=NULL, CPDAG=TRUE,
 #' @param cluster_results Cluster results
 #' @param data data
 #' @param node_colours node colours
+#' @param binary_cols Input can be a vector of the binary variables; if "TRUE", the binary variables are automatically determined; if NULL, then entropy will be used only
 #'
 #' @export
 #' @importFrom igraph graph_from_adjacency_matrix
 #' @importFrom ggpubr ggarrange
-plot_clusters <- function(cluster_results, data=NULL, node_colours="#fdae61"){
-
+plot_clusters <- function(cluster_results, data=NULL, node_colours="#fdae61", binary_cols = TRUE){
+  
   node_size <- NULL
   # if entropy should define the size
   if (!is.null(data)){
     k_clust <- length(cluster_results$DAGs)
-
-    # n_net <- 1
-    # top_entropy <- list()
+    
     # calculate overall entropy
     total_entropies <- sapply(mut_cov_data, function(x) entropy(x))
     # calculate relative entropy and select variables with the lowest entropy
@@ -99,13 +108,57 @@ plot_clusters <- function(cluster_results, data=NULL, node_colours="#fdae61"){
       # all_var_selected <- unique(c(all_var_selected, var_selection))
       # top_entropy[[nn]] <- var_selection
     }
-
+    
     # calculate size
     all_var_selected <- 1:dim(data)[2]
     node_size_percentage <- 1-(diff_entropies[,all_var_selected]+abs(min(diff_entropies[,all_var_selected])))/(max(diff_entropies[,all_var_selected])+abs(min(diff_entropies[,all_var_selected])))
     node_size <- 1.5+node_size_percentage*6
+    
+    # calculate overall entropy
+    total_entropies <- sapply(mut_cov_data, function(x) entropy(x))
+    # calculate relative entropy and select variables with the lowest entropy
+    n_net <- 7 # number of selected variables per cluster
+    all_var_selected <- c()
+    diff_entropies <- matrix(NA, nrow = k_clust, ncol = ncol(mut_cov_data))
+    binary_frequency <- matrix(NA, nrow = k_clust, ncol = ncol(mut_cov_data))
+    for (nn in 1:k_clust){
+      # select data
+      cluster_data <- mut_cov_data[cluster_results$clustermembership==nn,]
+      # calculate entropy in clusters
+      cluster_entropies <- sapply(cluster_data, function(x) entropy(x))
+      # calculate relative entropy
+      diff_entropies[nn,] <- cluster_entropies-total_entropies
+      # calculate frequency
+      binary_frequency[nn,] <- sapply(cluster_data, function(x) sum(x))
+      # # # select variables with lowest relative entropy
+      # # var_selection <- order(diff_entropies[nn,])[1:n_net]
+      # # all_var_selected <- unique(c(all_var_selected, var_selection))
+      # # select variables with highest frequency
+      # var_selection <- order(binary_frequency[nn,], decreasing = TRUE)[1:n_net]
+      # all_var_selected <- unique(c(all_var_selected, var_selection))
+    }
+    
+    # set node size for entropy
+    node_size_percentage <- 1-(diff_entropies[,]+abs(min(diff_entropies[,])))/(max(diff_entropies[,])+abs(min(diff_entropies[,])))
+    node_size <- 1.5+node_size_percentage*6
+    
+    # if there are binary variables, integrate them
+    if(!is.null(binary_cols)){
+      if(binary_cols==TRUE){
+        binary_cols <- as.vector(which(colSums(cluster_data>1)==0))
+      }
+      # set cols of genomic information and set frequ to zero elsewhere
+      binary_frequency[,-binary_cols] <- 0
+      
+      # set node size for frequency
+      node_size_percentage_frequ <- (binary_frequency[,]+abs(min(binary_frequency[,])))/(max(binary_frequency[,])+abs(min(binary_frequency[,])))
+      node_size_frequ <- 1.5+node_size_percentage_frequ*6
+      
+      # merge
+      node_size[,binary_cols] <- node_size_frequ[,binary_cols]
+    }
   }
-
+  
   # plot DAGs of each cluster
   p_list <- list()
   k_clust <- length(cluster_results$DAGs)
@@ -119,12 +172,58 @@ plot_clusters <- function(cluster_results, data=NULL, node_colours="#fdae61"){
 }
 
 
-# Compute by entropy
-entropy <- function(cat.vect){
-  # from https://stats.stackexchange.com/questions/221332/variance-of-a-distribution-of-multi-level-categorical-data
-  px  = table(cat.vect)/length(cat.vect)
-  lpx = log(px, base=2)
-  ent = -sum(px*lpx)
-  return(ent)
-}
-
+# #' @title plot_clusters
+# #'
+# #' @description Plot clusters
+# #'
+# #' @param cluster_results Cluster results
+# #' @param data data
+# #' @param node_colours node colours
+# #'
+# #' @export
+# #' @importFrom igraph graph_from_adjacency_matrix
+# #' @importFrom ggpubr ggarrange
+# plot_clusters <- function(cluster_results, data=NULL, node_colours="#fdae61"){
+# 
+#   node_size <- NULL
+#   # if entropy should define the size
+#   if (!is.null(data)){
+#     k_clust <- length(cluster_results$DAGs)
+# 
+#     # n_net <- 1
+#     # top_entropy <- list()
+#     # calculate overall entropy
+#     total_entropies <- sapply(mut_cov_data, function(x) entropy(x))
+#     # calculate relative entropy and select variables with the lowest entropy
+#     all_var_selected <- c()
+#     diff_entropies <- matrix(NA, nrow = k_clust, ncol = ncol(data))
+#     for (nn in 1:k_clust){
+#       # select data
+#       cluster_data <- data[cluster_results$clustermembership==nn,]
+#       # calculate entropy in clusters
+#       cluster_entropies <- sapply(cluster_data, function(x) entropy(x))
+#       # calculate relative entropy
+#       diff_entropies[nn,] <- cluster_entropies-total_entropies
+#       # select variables with lowest relative entropy
+#       # var_selection <- order(diff_entropies[nn,])[1:n_net]
+#       # all_var_selected <- unique(c(all_var_selected, var_selection))
+#       # top_entropy[[nn]] <- var_selection
+#     }
+# 
+#     # calculate size
+#     all_var_selected <- 1:dim(data)[2]
+#     node_size_percentage <- 1-(diff_entropies[,all_var_selected]+abs(min(diff_entropies[,all_var_selected])))/(max(diff_entropies[,all_var_selected])+abs(min(diff_entropies[,all_var_selected])))
+#     node_size <- 1.5+node_size_percentage*6
+#   }
+# 
+#   # plot DAGs of each cluster
+#   p_list <- list()
+#   k_clust <- length(cluster_results$DAGs)
+#   for (ii in 1:k_clust){
+#     # my_graph <- graph_from_adjacency_matrix(cluster_results$DAGs[ii][[1]], mode="directed")
+#     my_DAG <- cluster_results$DAGs[ii][[1]]
+#     # my_graph <- igraph::graph_from_adjacency_matrix(cluster_results$DAGs[ii][[1]], mode="directed")
+#     p_list[[ii]] <- nice_DAG_plot(my_DAG, print_direct=FALSE, node_size=node_size[ii,], node_colours=node_colours)
+#   }
+#   ggarrange(plotlist=p_list, labels = paste("Cluster", LETTERS[1:k_clust]))#, ncol = 2, nrow = 2)
+# }
