af9bea6ed3fa3a730f88a65b08bd28eef78cd971
PhysFritz <fritzbayer@ymail.com>
2023-07-13 14:37:36 +0200

    tesst

--- test_delete_later.R
+++ test_delete_later.R
@@ -1,5 +1,5 @@
-function (scorepar, incidence, datatoscore = NULL, marginalise = FALSE, 
-          onlymain = FALSE, bdecatCvec = NULL) 
+function (scorepar, incidence, datatoscore = NULL, marginalise = FALSE,
+          onlymain = FALSE, bdecatCvec = NULL)
 {
   if (onlymain & !marginalise) {
     n <- scorepar$n
@@ -24,11 +24,11 @@ function (scorepar, incidence, datatoscore = NULL, marginalise = FALSE,
     }
   }
   if (scorepar$type == "bge" && marginalise != FALSE) {
-    return(scoreagainstDAGmargBGe(n, scorepar, incidence, 
+    return(scoreagainstDAGmargBGe(n, scorepar, incidence,
                                   datatoscore))
   }
   else if (scorepar$type == "mixed") {
-    binscore <- scoreagainstDAG(scorepar$binpar, incidence[1:scorepar$nbin, 
+    binscore <- scoreagainstDAG(scorepar$binpar, incidence[1:scorepar$nbin,
                                                            1:scorepar$nbin])
     gausscore <- scoreagainstDAG(scorepar$gausspar, incidence)
     return(binscore + gausscore)
@@ -37,7 +37,7 @@ function (scorepar, incidence, datatoscore = NULL, marginalise = FALSE,
     samplescores <- matrix(0, nrow = nrow(datatoscore), ncol = n)
     for (j in mainnodes) {
       parentnodes <- which(incidence[, j] == 1)
-      samplescores[, j] <- scoreagainstDAGcore(j, parentnodes, 
+      samplescores[, j] <- scoreagainstDAGcore(j, parentnodes,
                                                n, scorepar, datatoscore)
     }
   }
@@ -48,58 +48,10 @@ function (scorepar, incidence, datatoscore = NULL, marginalise = FALSE,
     samplescores <- matrix(0, nrow = nrow(datatoscore), ncol = n)
     for (j in 1:n) {
       parentnodes <- which(incidence[, j] == 1)
-      samplescores[, j] <- scoreagainstDAGcore(j, parentnodes, 
+      samplescores[, j] <- scoreagainstDAGcore(j, parentnodes,
                                                n, scorepar, datatoscore)
     }
   }
   return(rowSums(samplescores))
 }
 
-function(scorepar, incidence, datatoscore=NULL, marginalise=FALSE, bdecatCvec=NULL){
-  
-  n<-scorepar$n
-  
-  if (is.null(datatoscore)) {
-    datatoscore<-scorepar$data
-  }
-  
-  if(is.vector(datatoscore)){ # if input is a vector
-    datatoscore <- matrix(datatoscore, nrow=1) # cast it as a matrix
-  }
-  
-  if (scorepar$type=="bge") {
-    if(marginalise==FALSE){
-      datatoscore <- t(t(datatoscore) - scorepar$muN) # recentre around posterior mean
-    } else {
-      datatoscore <- t(t(datatoscore) - scorepar$means) # recentre about data mean
-    }
-  }
-  
-  if (scorepar$type=="bge" && marginalise!=FALSE){
-    return(BiDAG:::scoreagainstDAGmargBGe(n, scorepar, incidence, datatoscore))
-  } else if (scorepar$type=="mixed") {
-    binscore<-scoreagainstDAG(scorepar$binpar, incidence[1:scorepar$nbin,1:scorepar$nbin])
-    gausscore<-scoreagainstDAG(scorepar$gausspar, incidence)
-    return(binscore+gausscore)
-    
-  } else if (scorepar$type=="bde"){
-    samplescores <- matrix(0,nrow=nrow(datatoscore),ncol=n)
-    for (j in 1:n)  {
-      parentnodes <- which(incidence[,j]==1)
-      samplescores[,j]<-scoreagainstDAGcore(j,parentnodes,n,scorepar,datatoscore)
-    }
-    
-    return(rowSums(samplescores))
-  } else {
-    if (!is.null(bdecatCvec)) {
-      scorepar$Cvec <- bdecatCvec
-    }
-    samplescores <- matrix(0,nrow=nrow(datatoscore),ncol=n)
-    for (j in 1:n)  {
-      parentnodes <- which(incidence[,j]==1)
-      samplescores[,j]<-scoreagainstDAGcore(j,parentnodes,n,scorepar,datatoscore)
-    }
-    
-    return(rowSums(samplescores))
-  }
-}
